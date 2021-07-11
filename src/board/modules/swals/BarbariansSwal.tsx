a6de36e7e086f89330f544db9482c54bda8b0052
Simon Pasquereau <spasquereau@vectice.com>
2021-07-11 15:57:15 +0200

    Fix

--- src/board/modules/swals/BarbariansSwal.tsx
+++ src/board/modules/swals/BarbariansSwal.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import useMount from '@hooks/useMount';
 import barbarians from '@images/barbarians.png';
 
 import './BarbariansSwal.less';
@@ -9,9 +10,19 @@ interface Props {
 }
 
 const BarbariansSwal = ({ attack }: Props) => {
+  const [imgLoaded, setImageLoaded] = useState(false);
+
+  useMount(() => {
+    const image = new Image();
+    image.onload = () => setImageLoaded(true);
+    image.src = barbarians;
+  });
+
+  if (!imgLoaded) return <div className="barbarians-swal" />;
+
   return (
     <div className="barbarians-swal">
-      <img src={barbarians} alt="Vilain barbarians" />
+      {<img src={barbarians} alt="Vilain barbarians" />}
       <p>{attack ? 'Les barbares attaquent !' : 'Les barbares avancent !'}</p>
     </div>
   );
--- src/board/modules/swals/ThiefSwal.tsx
+++ src/board/modules/swals/ThiefSwal.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import useMount from '@hooks/useMount';
 import thief from '@images/brigand.png';
 
 import './ThiefSwall.less';
 
 const ThiefSwal = () => {
+  const [imgLoaded, setImageLoaded] = useState(false);
+
+  useMount(() => {
+    const image = new Image();
+    image.onload = () => setImageLoaded(true);
+    image.src = thief;
+  });
+
+  if (!imgLoaded) return <div className="barbarians-swal" />;
   return (
     <div className="thief-swal">
       <img src={thief} alt="This is a vilain thief" />
