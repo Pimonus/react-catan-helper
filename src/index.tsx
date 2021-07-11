a72db82701e1dfe24492f9108688e9327888fbc8
Simon Pasquereau <spasquereau@vectice.com>
2021-07-11 14:36:51 +0200

    Remove completely antd library

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import 'antd/dist/antd.css';
 import 'sanitize.css/sanitize.css';
 
 import Board from '@board/Board';
